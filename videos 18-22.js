40c4343d004f7884dbaa470e6a56e917f396452c
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-04-25 01:08:39 +0200

    ass 2

--- videos 18-22.js
+++ videos 18-22.js
@@ -1,3 +1,27 @@
 // Assiment 1
 
 console.log((10 - 20 + (15 - 3)) * (190 + 10) - 400);
+
+//----------------------------------------------------
+
+//Assiment 2
+
+let num = 3;
+
+// Solution One
+console.log(3 + 3); // 6
+
+// Solution Two
+console.log(num + num); // 6
+
+// Soultion Three
+console.log(num ** 2 - 3); // 6
+
+// Soultion Four
+console.log(true * 6); // 6
+
+// Solution Five
+console.log(num + 2 + true); // 6
+
+// Solution Six
+console.log(true + true + true + true + true + true); // 6
