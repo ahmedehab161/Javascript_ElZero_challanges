ef5c608b04f95e50119011b5accdb686074895c3
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-04-24 06:32:21 +0200

    Assi 1 Finished

--- videos 10-17.js
+++ videos 10-17.js
@@ -1,1 +1,14 @@
-document.write("<h5>Ahmed ehab</h5>")
+let NumberOne = 10;
+let NumberTwo = 20;
+
+console.log("10" + "20");
+console.log(NumberOne + "" + NumberTwo);
+console.log(`${"10"}${"20"}`);
+console.log(`${NumberOne}${NumberTwo}`);
+
+console.log(NumberTwo + "\n" + NumberOne);
+console.log(`${NumberTwo}
+${NumberOne}`);
+
+//-------------------------------------------------------
+
