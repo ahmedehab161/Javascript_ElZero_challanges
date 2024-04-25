60e176bf8166112b5527290639f505c02fbd29a2
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-04-25 09:00:49 +0200

    assiment 1

---
+++ videos 23-26.js
@@ -0,0 +1,23 @@
+// Assiment 1
+
+console.log(100_000); // 100000
+console.log(1e5); // 100000
+console.log(5e4 + 5e4); // 100000
+console.log(100000); // 100000
+console.log(Number("50000") + Number("50000")); // 100000
+console.log(10 * 10000); // 100000
+console.log(String("100000")); // 100000
+console.log(10 * 10 * 10 * 10 * 10); // 100000
+console.log(Math.pow(10 , 5)); // 100000
+console.log(10 ** 5); // 100000
+
+//----------------------------------------------------
+// Assiment 2
+
+
+// Assiment 3
+// Assiment 4
+// Assiment 5
+// Assiment 6
+// Assiment 7
+
