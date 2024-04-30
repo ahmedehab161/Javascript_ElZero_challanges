dda8b704faed99582844f09aa45bbd7d12250765
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-04-30 08:11:02 +0300

    ass. 2

--- videos 27-30.js
+++ videos 27-30.js
@@ -7,3 +7,14 @@ console.log(userName.substring(0 , 1).toLowerCase()); // e
 console.log(userName.substr(0 , 1) .toLowerCase()); // e
 console.log(userName.charAt(0).toLowerCase()); // e
 console.log(userName[0].toLowerCase().repeat(3)); // eee
+
+// Assiment 2
+
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(userName.includes(letterZ)); // True
+console.log(userName.startsWith(letterE.toUpperCase())); // True
+console.log(userName.endsWith(letterO.toLowerCase())); // True
