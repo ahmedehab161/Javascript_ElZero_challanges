9d2a928fef649f5601614a01ce1e5c1bfbce63a7
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-05-02 03:37:26 +0300

    video 47.js init.

---
+++ video 47.js
@@ -0,0 +1,21 @@
+/* 
+    Arrays Challenges
+*/
+
+// Only Use The Vars Don't Use Any Number From Outside The Vars Like EX. (When Using Slice())
+
+let zero = 0;
+
+let counter = 3;
+
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+
+//Write Code Here
+
+console.log(my); //["Osama" , "Elham" , "Mazero" , "Ahmed"]
+
+console.log(my.slice("?????")); //["Elham" , "Mazero"]
+
+console.log(); // "Elzero"
+
+console.log(); //"rO"
