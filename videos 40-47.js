3420bf13f53ecdd03a6e8b0d9184c248d7df01f7
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-05-07 17:17:16 +0300

    Assignment 4 / 5 and 6 init.

--- videos 40-47.js
+++ videos 40-47.js
@@ -28,8 +28,22 @@ finalArr = [arrTwo.pop() , arrOne.pop() , arrOne.reverse() , arrTwo.reverse()];
 console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
 //--------------------------------------------------------------------------------------------------------
 //Assignment 4
+let website = "Go";
+let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
 
+console.log(words[website.length][words.length - words.length].slice(2).toUpperCase()); // ZERO
 //--------------------------------------------------------------------------------------------------------
 //Assignment 5
+let needle = "JS";
+let haystack = ["PHP", "JS", "Python"];
+
+// Write 3 Solutions
 //--------------------------------------------------------------------------------------------------------
 //Assignment 6
+// let arr1 = ["A", "C", "X"];
+// let arr2 = ["D", "E", "F", "Y"];
+// let allArrs = [];
+
+// // Your Code Here
+
+// console.log(allArrs); // fxy
