a3871a1864b4b4fd652e7aec1439c606abf5cbea
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-05-07 14:57:30 +0300

    assignment 1 , 2 and 3 init.

--- videos 40-47.js
+++ videos 40-47.js
@@ -4,15 +4,29 @@ let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
 let num = 3;
 
 // Method 1
-console.log(""); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.slice((num - num) , num)); // ["Ahmed", "Elham", "Osama"];
 
 // Method 2
-console.log("Your Code Here"); // ["Ahmed", "Elham", "Osama"];
-
+myFriends.pop()
+console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
 //--------------------------------------------------------------------------------------------------------
 //Assignment 2
+let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
+
+// Write Your Code Here
+friends.shift() + friends.pop();
+
+console.log(friends); // ["Eman", "Osama"]
 //--------------------------------------------------------------------------------------------------------
 //Assignment 3
+let arrOne = ["C", "D", "X"];
+let arrTwo = ["A", "B", "Z"];
+let finalArr = [];
+
+// Write One Single Line Of Code
+
+
+console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
 //--------------------------------------------------------------------------------------------------------
 //Assignment 4
 //--------------------------------------------------------------------------------------------------------
