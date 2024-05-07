51ae43e76eae2ac056ca0fe269cb35cae6f88112
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-05-07 03:47:08 +0300

    video 47 all challange finished

--- video 47.js
+++ video 47.js
@@ -12,10 +12,27 @@ let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
 
 //Write Code Here
 
-console.log(my); //["Osama" , "Elham" , "Mazero" , "Ahmed"]
+let newMy = my.pop() + my.pop();
+console.log(my.reverse()); //["Osama" , "Elham" , "Mazero" , "Ahmed"]
 
-console.log(my.slice("?????")); //["Elham" , "Mazero"]
+//---------------------------------------------------------------------------------------
+//It also can be solved with (Min() and Max())
+let startIndex = counter - (--counter);
 
-console.log(); // "Elzero"
+/*
+                    Warning
+Now because we decrement counter in the previous code 
+counter now is = 2
+*/ 
 
-console.log(); //"rO"
+let endIndex = ++counter;
+
+console.log(my.slice(startIndex, endIndex)); //["Elham", "Mazero"]
+
+//------------------------------------------------------------------------------------------------
+// ** This the Fastest Solution There Are Many Other Solutions **
+console.log(my[1][0] + my[1][1] + my[2][2] + my[2][3] + my[2][4] + my[2][5]); // "Elzero"
+
+//-----------------------------------------------------------------------------------------------
+// ** This the Fastest Solution There Are Many Other Solutions **
+console.log(my[2][4] + my[0][0]); //"rO"
