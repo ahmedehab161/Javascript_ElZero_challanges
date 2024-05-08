01d4e165fa0e62a2ae47a90d70c4e595caa07a97
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-05-08 17:28:53 +0300

    video 53 init.

---
+++ video 53.js
@@ -0,0 +1,9 @@
+/*
+    This Is A Parctice Video And It Is Already Solved
+    In The Video
+*/
+
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor", "iPhone"];
+let colors = ["Red", "Green", "Blue"];
+let showCount = 5;
+
