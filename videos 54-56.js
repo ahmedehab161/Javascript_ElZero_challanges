9de1fe5562bd76785a381f19e50f75453192b52d
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-05-12 03:10:33 +0300

    videos from 54 to 56

---
+++ videos 54-56.js
@@ -0,0 +1,9 @@
+//Assignment 1
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index = 0;
+let counter = 0;
+
+// Output
+("1 => Sayed");
+("2 => Mahmoud");
+//-------------------------------------------------------------------------------
