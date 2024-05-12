7a5474eb39c43308d724946d49deb503dbf000b5
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-05-13 01:39:51 +0300

    file removed

--- videos 54-56.js
+++
@@ -1,9 +0,0 @@
-//Assignment 1
-let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
-let index = 0;
-let counter = 0;
-
-// Output
-("1 => Sayed");
-("2 => Mahmoud");
-//-------------------------------------------------------------------------------
