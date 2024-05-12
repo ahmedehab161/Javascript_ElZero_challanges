4069c08fd9d8549fb5f0cda773ab108eb4f1e309
Ahmed Ehab <ahmedmedo012556@gmail.com>
2024-05-12 18:44:08 +0300

    video 56 challenge

--- video 56.js
+++ video 56.js
@@ -7,16 +7,24 @@ let myEmployees = ["Amgad", "Samah", "Ameer", "Omar", "Othman", "Amany", "Samia"
 
 let index = 0;
 while (index < myAdmins.length) {
-    if (myAdmins[index] == "stop") {
+    if (myAdmins[index] == "Stop") {
         break;
     }
     index++;
 }
 document.write(`<div>We Have ${index} Admins</div>`);
 document.write(`<hr>`);
-
-document.write(`<div> The Admin For Team {x} Is {x}`);
-document.write(`<h3>Team Members:</h3>`);
-document.write(`<p>-</p>`);
-document.write(`</div>`);
-document.write(`<hr>`);
+for (let i = 0; i < index; i++)
+{
+    document.write(`<div> The Admin For Team ${i + 1} Is ${myAdmins[i]}`);
+    document.write(`<h3>Team Members:</h3>`);
+    for (let j = 0; j < myEmployees.length; j++)
+    {
+        if (myEmployees[j].charAt(0) == myAdmins[i].charAt(0))
+        {
+            document.write(`<p>- ${myEmployees[j]}</p>`);
+        }
+    }
+    document.write(`</div>`);
+    document.write(`<hr>`);
+}
